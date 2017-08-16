a9aa6c7228b7ae392775f7e93e9876716a2beec6
СЕРГЕЙ-ПК\Сергей <babichsm@gmail.com>
2017-08-16 18:57:38 +0300

    Opening lists and changing the + sign to -

--- portal/js/toggle.js
+++ portal/js/toggle.js
@@ -3,5 +3,17 @@ $(document).ready(function () {
 		var $parent = $(this).parent();
 
 		$parent.find('> ul').toggle();
+		$parent.find('.plus').first().toggleClass('minus');
+
+		// if($parent.css('display') === 'none'){
+		// 	$('.submenu ul').css('display', 'none');
+		// }
 	});
+	$('.submenu > a').on('click', function(){
+		$('.submenu > a').removeClass('active');
+		var active = $(this).first();
+
+		active.addClass('active');
+		active.prev('.plus').hide();
+	})
 });
