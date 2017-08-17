457e5d8179537d9a35ed2a5f2cab0a7c1adc0934
СЕРГЕЙ-ПК\Сергей <babichsm@gmail.com>
2017-08-17 21:39:05 +0300

    Added functionality

--- portal/js/toggle.js
+++ portal/js/toggle.js
@@ -13,14 +13,19 @@ $(document).ready(function () {
 		}
 	});
 	$('.submenu > a').on('click', function(){
-		$('.submenu > a').removeClass('active');
+		$('li > a').removeClass('active');
 		var active = $(this).first();
 
 		active.addClass('active');
-		active.prev('.plus').toggle();
+		active.prev('.plus').hide().parent().parent().parent().find('> span').addClass('prev').css('display', 'block');
 
 		$(this).parent().parent().find('> li:not(:has(a.active))').hide();
 		$(this).parent().find('> ul').show();
 		$(this).parent().find(' li').show();
+
+		if($(this).parent().find('a.active').next('ul').length > 0){
+			$(this).parent().find('ul').children().first().find('.plus').css('display', 'block');
+			$(this).parent().find('ul').children().first().find('ul').css('display', 'none');
+		}
 	})
 });
